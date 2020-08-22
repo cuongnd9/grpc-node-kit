d7e6aad2cab6eda5134901bdf974c06f840b3abe
Cuong Tran <103cuong@gmail.com>
2020-08-23 03:01:20 +0700

    feat: add some bug

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -4,14 +4,14 @@ cd ..
 
 # JavaScript code generating
 grpc_tools_node_protoc \
---js_out=import_style=commonjs,binary:./src/rpc/models \
---grpc_out=generate_package_definition:./src/rpc/models \
+--js_out=import_style=commonjs,binary:./src/protos \
+--grpc_out=generate_package_definition:./src/protos \
 -I ./protos \
 ./protos/*.proto
 
 # generate d.ts codes
 grpc_tools_node_protoc \
 --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
---ts_out=generate_package_definition:./src/rpc/models \
+--ts_out=generate_package_definition:./src/protos \
 -I ./protos \
 ./protos/*.proto
